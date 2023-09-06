a6dd6da179185d0758475d876d6dd34d85336b73
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-05 19:29:30 -0700

    Add vector struct

---
+++ src/structs/vector.js
@@ -0,0 +1,30 @@
+const vector = (x, y) => ({ x, y })
+
+const add = (v1, v2) => vector(v1.x + v2.x, v1.y + v2.y)
+
+const subtract = (v1, v2) => vector(v1.x - v2.x, v1.y - v2.y)
+
+const scalarProduct = (v, c) => vector(v.x * c, v.y * c)
+
+const magnitude = (v) => Math.sqrt(v.x**2 + v.y**2)
+
+const dotProduct = (v1, v2) => v1.x * v2.x + v1.y * v2.y
+
+const angle = (v1, v2) => Math.acos(dot(v1, v2) / (magnitude(v1) * magnitude(v2)))
+
+// scalar component of v2 in direction of v1
+const scalarComponent = (v1, v2) => dot(v1, v2) / magnitude(v1)
+
+const crossProduct = (v1, v2) => v1.x*v2.y - v1.y*v2.x
+
+export const Vector = {
+    vector,
+    add,
+    subtract,
+    scalarProduct,
+    magnitude,
+    dotProduct,
+    angle,
+    scalarComponent,
+    crossProduct
+}
